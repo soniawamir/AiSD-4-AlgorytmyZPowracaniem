56dcb45c92b5e1fa61dc6493f5d6649d6430ec7f
krxychu69 <xkrxychu@gmail.com>
2026-05-24 20:13:44 +0200

    Add Euler cycle implementation
    Implemented Hierholzer's algorithm to find the Eulerian cycle using a stack. Added the required <stack> library.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <algorithm>
 #include <cmath>
+#include <stack>
 
 void addEdge(int node_1, int node_2, std::vector<std::vector<int>>& graph)
 {
@@ -118,6 +119,54 @@ void findHamiltonCycle(const std::vector<std::vector<int>>& graph, int nodes){
     }
 }
 
+void findEulerCycle(const std::vector<std::vector<int>>& original_graph, int nodes) {
+    for(int i = 0; i < nodes; i++) {
+        if(original_graph[i].size() % 2 != 0) {
+            std::cout << "Eulerian cycle not found (vertex " << i + 1 << " has an odd degree)." << std::endl;
+            return;
+        }
+    }
+
+    std::vector<std::vector<int>> graph = original_graph;
+    std::stack<int> curr_path;
+    std::vector<int> circuit;
+
+    int curr_v = 0;
+    curr_path.push(0);
+
+    while (!curr_path.empty()) {
+        if (!graph[curr_v].empty()) {
+
+            curr_path.push(curr_v);
+            int next_v = graph[curr_v].back();
+            graph[curr_v].pop_back();
+
+            auto it = std::find(graph[next_v].begin(), graph[next_v].end(), curr_v);
+            if (it != graph[next_v].end()) {
+                graph[next_v].erase(it);
+            }
+
+            curr_v = next_v;
+        } else {
+            circuit.push_back(curr_v);
+            curr_v = curr_path.top();
+            curr_path.pop();
+        }
+    }
+    for (int i = 0; i < nodes; i++) {
+        if (!graph[i].empty()) {
+            std::cout << "Eulerian cycle not found (graph is disconnected)." << std::endl;
+            return;
+        }
+    }
+    std::cout << "Eulerian cycle:\n";
+    for (int i = circuit.size() - 1; i >= 0; i--) {
+        std::cout << circuit[i] + 1;
+        if (i > 0) std::cout << " --> ";
+    }
+    std::cout << std::endl;
+}
+
 void exportToTikZ(int nodes, const std::vector<std::vector<int>>& graph)
 {
     const double PI = 3.1415;
@@ -224,8 +273,8 @@ int main(int argc, char* argv[])
             print(nodes, graph);
         } else if(action == "Export" || action == "export"){
             exportToTikZ(nodes, graph);
-        } else if(action == "Euler" || action == "euler"){
-
+		} else if(action == "Euler" || action == "euler"){
+            findEulerCycle(graph, nodes);
         } else if(action == "Hamilton" || action == "hamilton"){
             findHamiltonCycle(graph, nodes);
         } else {
